3aa857d29eb1037ee3a9caa079123e69a4f2a561
SimonZoo <simonzoo@yeah.net>
2019-01-08 19:58:38 +0800

    Add binary search

--- problem0167/two_sum_ii_input_array_is_sorted.py
+++ problem0167/two_sum_ii_input_array_is_sorted.py
@@ -5,9 +5,21 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        dict_nums = {}
+        
+        # dict_nums = {}
+        # for i, number in enumerate(numbers):
+        #     p = target - number
+        #     if p in dict_nums:
+        #         return [dict_nums[p] + 1, i + 1]
+        #     dict_nums[number] = i
         for i, number in enumerate(numbers):
             p = target - number
-            if p in dict_nums:
-                return [dict_nums[p] + 1, i + 1]
-            dict_nums[number] = i
+            low, high = i + 1, len(numbers) - 1
+            while low <= high:
+                mid = low + (high - low) // 2
+                if numbers[mid] == p:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < p:
+                    low = mid + 1
+                else:
+                    high = mid - 1
