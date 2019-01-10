eefa3b80e005d3177be1093a55498dc8a125ddde
SimonZoo <simonzoo@yeah.net>
2019-01-10 19:22:06 +0800

    Add another way

--- problem0167/two_sum_ii_input_array_is_sorted.py
+++ problem0167/two_sum_ii_input_array_is_sorted.py
@@ -5,21 +5,35 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        
+        # dictionary
         # dict_nums = {}
         # for i, number in enumerate(numbers):
         #     p = target - number
         #     if p in dict_nums:
         #         return [dict_nums[p] + 1, i + 1]
         #     dict_nums[number] = i
-        for i, number in enumerate(numbers):
-            p = target - number
-            low, high = i + 1, len(numbers) - 1
-            while low <= high:
-                mid = low + (high - low) // 2
-                if numbers[mid] == p:
-                    return [i + 1, mid + 1]
-                elif numbers[mid] < p:
-                    low = mid + 1
-                else:
-                    high = mid - 1
+
+
+        # another binary search
+        # for i, number in enumerate(numbers):
+        #     p = target - number
+        #     low, high = i + 1, len(numbers) - 1
+        #     while low <= high:
+        #         mid = low + (high - low) // 2
+        #         if numbers[mid] == p:
+        #             return [i + 1, mid + 1]
+        #         elif numbers[mid] < p:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+
+        low, high = 0, len(numbers) - 1
+        while numbers[low] + numbers[high] != target:
+            if numbers[low] + numbers[high] > target:
+                high -= 1
+            else:
+                low += 1
+        
+        return [low + 1, high + 1]
+            
+            
